66766ca5f1f5f658a338f4293f80428bf9ae7705
George Musker <george.musker@and.digital>
2022-10-28 14:04:45 +0100

    cors, and frontend start

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [organisations, setOrganisations] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/organisation")
+      .then((response) => response.data)
+      .then((data) => {
+        setOrganisations(data.organizations);
+      });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Organisation</h1>
+      <div></div>
+    </>
   );
 }
 
--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
-import express from 'express';
-import router from './routes/routes.js';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import express from "express";
+import router from "./routes/routes.js";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
 
 const corsOptions = {
   origin: "*",
@@ -19,11 +23,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use('/api', router);
+app.use("/api", router);
 
-console.log('ENV VAR: ', process.env.EVENTBRITE_BASE_URL)
-
+console.log("ENV VAR: ", process.env.EVENTBRITE_BASE_URL);
 
 app.listen(8080, () => {
-    console.log('server listening on port 8080');
+  console.log("server listening on port 8080");
 });
